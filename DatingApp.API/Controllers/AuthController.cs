4f52bfa4c4b6c629a0e7e61e185408eae3c72245
Kevin Stradtman <stradtkt@gmail.com>
2018-12-06 17:37:54 -0500

    Added login

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
@@ -16,26 +19,44 @@ namespace DatingApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
+            _config = config;
             _repo = repo;
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(UserForRegisterDto userFor) 
+        public async Task<IActionResult> Register(UserForRegisterDto userFor)
         {
             userFor.Username = userFor.Username.ToLower();
-            if(await _repo.UserExists(userFor.Username))
+            if (await _repo.UserExists(userFor.Username))
             {
                 return BadRequest("Username already exists");
             }
-            var userToCreate = new User 
+            var userToCreate = new User
             {
                 Username = userFor.Username
             };
             var createdUser = await _repo.Register(userToCreate, userFor.Password);
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userFor)
+        {
+            var userFromRepo = await _repo.Login(userFor.Username, userFor.Password);
+            if (userFromRepo == null)
+            {
+                return Unauthorized();
+            }
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
         }
     }
 }
